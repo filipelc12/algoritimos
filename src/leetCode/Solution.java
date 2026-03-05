108a78f7249fbe86282c2f7d8d64d7df4f496012
Filipe <filipelc12@gmail.com>
2026-03-05 20:13:35 -0300

    primeiro commit

---
+++ src/leetCode/Solution.java
@@ -0,0 +1,40 @@
+package leetCode;
+
+public class Solution{
+
+ /*
+ Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+CASE 1
+[2,7,11,15]
+9
+  */
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] retorno = new int[2];
+        for(int i = 0; i <= nums.length -1; i++){
+            for(int iNside = i+1; iNside<= nums.length -1; iNside++){
+                int soma = nums[i] + nums[iNside];
+                IO.println("Ponteiro interno: " +nums[iNside]);
+                if( soma == target){
+                    retorno[0] = i;
+                    retorno[1] = iNside;
+                    IO.println("Soma correta: " +i +" + " +iNside);
+                    return retorno;
+                }
+                soma = 0;
+            }
+        }
+        IO.println("Target não atingido com inputs passados");
+        return null;
+
+    }
+
+    public Solution(){}
+
+
+}
---
+++ src/neetcode/designdynamicarray/DynamicArray.java
@@ -0,0 +1,83 @@
+package neetcode.designdynamicarray;
+
+// nao esquecer de remover esse public posteriormente
+public class DynamicArray {
+
+
+    private int size;
+    private int capacity;
+    private int[] meuArray;
+
+
+    public DynamicArray(int capacity) {
+        this.capacity = capacity;
+        this.meuArray = new int[capacity];
+    }
+
+    public int get(int i) {
+//        int retorno = 0;
+        try {
+            return meuArray[i];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return 0;
+        }
+    }
+
+    public void set(int i, int n) {
+        this.meuArray[i] = n;
+
+    }
+
+    public void pushback(int n) {
+
+        if(this.size >= this.capacity){
+            this.resize();
+        }
+        this.meuArray[this.size] = n;
+        this.size++;
+
+
+
+    }
+
+    public int popback() {
+        int removido;
+        int ultimoValor = this.size-1;
+        removido = this.meuArray[ultimoValor];
+        this.size --;
+        set(ultimoValor,0);
+        return removido;
+
+    }
+
+    public void resize() {
+        this.capacity = this.capacity * 2;
+        int[] arrayProvisorio = new int[this.capacity];
+        for(int i = 0; i < this.size; i++){
+            arrayProvisorio[i] = this.meuArray[i];
+        }
+        this.meuArray = new int[this.capacity];
+        this.meuArray = arrayProvisorio;
+
+    }
+
+    /*
+    RETORNA OS ELEMENTOS VALIDOS DO ARRAY
+     */
+    public int getSize() {
+        return this.size;
+    }
+
+    /*
+    RETORNA A CAPACIDADE DO ARRAY
+     */
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public void imprimeArray(){
+        for(int i = 0; i< this.meuArray.length; i++ ){
+            IO.println(this.meuArray[i]);
+        }
+    }
+}
