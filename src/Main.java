cf644772552bed26916dd07601903f9ddc00d770
Filipe <filipelc12@gmail.com>
2026-04-12 13:48:06 -0300

    essa foi hardcore

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 
+import leetCode.Algorithms;
 import leetCode.Solution;
 import neetcode.designdynamicarray.DynamicArray;
 
 public class Main {
 
   public static void main(String[] args){
+    Algorithms a = new Algorithms();
     //System.out.println(Algorithms.isPalindrome(13412));
 //
 //    int[] nums = {1,2,4,3,0,5};
 //
 //    System.out.println(Algoritimos.hasDuplicate(nums));
-    System.out.println(Algoritimos.isAnagram("cor", "roc"));
+    System.out.println(a.romanToInt("IX"));
 
 
 
--- src/leetCode/Algorithms.java
+++ src/leetCode/Algorithms.java
@@ -30,5 +30,50 @@ public class Algorithms{
 
     }
 
+    /*
+    Autoexplicativo, pega um romano e transforma e m numero
+    "MCMXCIV"
+     */
+
+    public int romanToInt(String s) {
+        int resultado = 0;
+
+        String[] numerosRomanos = {"I", "V", "X", "L", "C", "D", "M"};
+        //char[] romano = s.toCharArray();
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            int atual = romano(s.charAt(i));
+            int proximo = romano(s.charAt(i + 1));
+
+            if (atual < proximo) {
+                resultado -= atual; // Subtrai o menor que vem antes
+            } else {
+                resultado += atual; // Soma o maior (ou igual)
+            }
+        }
+        return resultado + romano(s.charAt(s.length()-1));
+    }
+
+    private int romano(char c){
+        switch (c){
+            case 'M':
+                return 1000;
+            case 'D':
+                return 500;
+            case 'C':
+                return 100;
+            case 'L':
+                return 50;
+            case 'X':
+                return 10;
+            case 'V':
+                return 5;
+            case 'I':
+                return 1;
+            default:
+                throw new RuntimeException("Numero invalido");
+        }
+    }
+
 
 }
